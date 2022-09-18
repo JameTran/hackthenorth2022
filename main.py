8d7753da1df76344c599f57f2f2f3886f7517be2
Jame Tran <jamedhtran@gmail.com>
2022-09-18 05:48:16 -0400

    added tone functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import summary
 import grammar
+import tones
 
 app = FastAPI()
 paragraphs = summary.readFile("sample.txt")
@@ -13,12 +14,22 @@ async def root():
 @app.get("/summary")
 async def summaryasync():
     summaries = summary.generateSummary(paragraphs)
-    return {summaries}
+    return summaries
     
-@app.get("/tone")
+@app.get("/informal_tone")
 async def tone():
-    return {"message": "placeholder, tones to be implemented"}
-
+    informal_paragraphs = []
+    for i in paragraphs:
+        informal_paragraphs.append(tones.informal_to_formal(i))
+    return informal_paragraphs
+'''
+@app.get("/formal_tone")
+async def tone():
+    formal_paragraphs = []
+    for i in paragraphs:
+        formal_paragraphs.append(tones.tones.formal_to_informal(i))
+    return formal_paragraphs
+'''
 @app.get("/correction")
 async def correction():
     corrected_paragraphs = []
